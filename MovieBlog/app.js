4680d08a260e7113568c927d60c0db2225fd3d7a
Burak Unuvar <burakunuvar@gmail.com>
2019-06-22 08:37:33 +0300

    Part6 - Add Authentication with Passport

--- MovieBlog/app.js
+++ MovieBlog/app.js
@@ -5,8 +5,13 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const Movie = require("./models/movie");
 const Comment = require("./models/comment");
+const User = require("./models/user");
 const seedDB = require("./seeds");
 const expressSanitizer = require('express-sanitizer');
+const session = require('express-session');
+const passport = require("passport");
+// const LocalStrategy  = require("passport-local");
+const passportLocalMongoose = require('passport-local-mongoose');
 
 seedDB();
 mongoose.connect('mongodb://localhost:27017/moviesDB', {useNewUrlParser: true});
@@ -20,6 +25,26 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(expressSanitizer());
+
+app.use(session({
+  secret: 'moviesDB-buraku',
+  resave: false,
+  saveUninitialized: false,
+  // cookie: { secure: true }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(User.createStrategy());
+// passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+ res.locals.currentUser = req.user ;
+ next();
+});
 
 app.get("/", (req, res) => {
   // console.log(__dirname) ;
@@ -80,7 +105,7 @@ app.get("/movies/:id",function(req,res){
 
 // ======== COMMENT ROUTES ARE NESTED STARTING HERE==========
 // 4.b New	    /movies/:id/comments/new
-app.get("/movies/:id/comments/new",(req,res)=>{
+app.get("/movies/:id/comments/new",isLoggedIn,(req,res)=>{
   console.log("**** Nested RESTAPI: 1.a GET request made to newCommentForm ");
   Movie.findById(req.params.id).populate("comments").exec(function(err,whichMovietoComment){
     if(!err){
@@ -93,7 +118,7 @@ app.get("/movies/:id/comments/new",(req,res)=>{
 });
 
 // 4.c Create	/movies/:id/comments
-app.post("/movies/:id/comments",(req,res)=>{
+app.post("/movies/:id/comments",isLoggedIn,(req,res)=>{
   console.log("**** Nested RESTAPI: 1.b POST request made to /movies/:id/comments through newCommentForm ");
   // console.log(req.body.comment);
   req.body.comment.text = req.sanitize(req.body.comment.text);
@@ -119,6 +144,82 @@ app.post("/movies/:id/comments",(req,res)=>{
   });
 });
 // ======== COMMENT ROUTES ARE NESTED - ENDING HERE ==========
+
+// ======== AUTH ROUTES START HERE  ==========
+
+//REGISTER
+app.get("/register", function(req, res) {
+ res.render("register");
+});
+
+app.post("/register", function(req, res) {
+ const newUser= new User({
+   username: req.body.username
+ });
+ User.register(newUser, req.body.password, function(err, user) {
+   if (err) {
+     console.log(err);
+     res.redirect("/register");
+   }else{
+     passport.authenticate('local')(req, res, function() {
+       res.redirect('/movies');
+     });
+   }
+ });
+});
+
+//LOGIN
+
+
+app.get("/login",function(req,res){
+    console.log("request to login/ ") ;
+    res.render("login");
+});
+
+// app.post("/login", passport.authenticate("local", {
+//         successRedirect: "/movies",
+//         failureRedirect: "/login"
+//     }) ,function(req, res){
+// });
+
+app.post("/login", function(req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.passport
+  });
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+      console.log("LOGIN FAILED");
+    }else{
+      passport.authenticate('local')(req, res, function() {
+        console.log("LOGIN SUCCESS");
+        res.redirect('/movies');
+      });
+    }
+  });
+});
+
+//LOGOUT
+
+app.get("/logout", function(req, res){
+   req.logout();
+   res.redirect("/");
+});
+
+// ======== AUTH ROUTES END HERE ==========
+
+// middleware function for authenticaton check
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    console.log("MIDDLEWARE RUNNING SUCCESSFULLY, NEXT");
+    return next();
+  }else{
+    console.log("MIDDLEWARE FAILED");
+    res.redirect("/login");
+  }
+}
+
 
 app.listen(3000, function() {
   console.log("MovieCamp started on port 3000");
